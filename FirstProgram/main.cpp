d9f3a5f108349456463f83b93335ff44ab1e3c44
Siddhartha Dhiman <sid.kill3r@gmail.com>
2021-12-12 14:48:32 -0500

    Add first program :)

---
+++ FirstProgram/main.cpp
@@ -0,0 +1,16 @@
+/*
+Program to take an input of number from 1 - 100 and valide user's feeling so
+they feel more secure.
+*/
+
+#include <iostream>
+
+int main()
+{
+    int favoriteNumber;
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    std::cin >> favoriteNumber;
+    std::cout << "Amazing! " << favoriteNumber << " is my favorite number too!"
+        << std::endl;
+    return 0;
+}
